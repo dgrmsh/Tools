8330e87edeaefa4f9ef8e4147dacf6eaa78943a7
dinix111 <GitHub@dinix111>
2013-09-14 23:21:32 +0300

    added config file parser

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ getquote.sh: prints a random quote from bash.im (in Russian),
              if put into ~/.profile, runs at every terminal launch
 
 ginboxchecker.cpp: returns the count of unread emails in the gmail INBOX;
-             the token should be refreshed before running, new mail is checked each 5 minutes
+             new mail is checked each 5 minutes
--- ginboxchecker.cpp
+++ ginboxchecker.cpp
@@ -1,10 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <string>
+#include <string.h>
 #include <unistd.h>
 #include </usr/local/include/curl/curl.h>
+
+CURLcode parseConfig(std::string *Email, std::string *ClientId, std::string *ClientSecret, std::string *RefreshToken) {
+  std::ifstream ifs("ginboxchecker.conf");
+  int lnumber=1;
+  if(!ifs.good()) {
+    printf("Could not open ginboxchecker.conf\n");
+    ifs.close();
+    return CURLE_READ_ERROR;
+  }
+  std::string line,name,value;
+  std::getline(ifs,line);
+  while(line.size()) {
+    int ind = line.find("=");
+    name = line.substr(0,ind);
+    value = line.substr(ind+2, line.size()-ind-4);
+    if(name.size()>0 && value.size()>0) {
+      if(name == "email") {
+        *Email = value;
+      } else if(name == "client_id") {
+        *ClientId = value;
+      } else if(name == "client_secret") {
+        *ClientSecret = value;
+      } else if(name == "refresh_token") {
+        *RefreshToken = value;
+      } else {
+        printf("Unknown parameter name in line '%s'\n",line.c_str());
+        ifs.close();
+        return CURLE_READ_ERROR;
+      }
+    } else {
+      printf("Line %d:'%s' is not of form name=value;\n",lnumber,line.c_str());
+      ifs.close();
+      return CURLE_READ_ERROR;
+    }
+    std::getline(ifs,line);
+    ++lnumber;
+  }
+  bool wrong=false;
+  if(!Email->size()) {
+     wrong = true;
+     printf("email is missing.\n");
+  }
+  if(!ClientId->size()) {
+     wrong = true;
+     printf("client_id is missing.\n");
+  }
+  if(!ClientSecret->size()) {
+     wrong = true;
+     printf("client_secret is missing.\n");
+  }
+  if(!RefreshToken->size()) {
+     wrong = true;
+     printf("refresh_token is missing.\n");
+  }
+  if(wrong) {
+     printf("Config file contains errors. Exiting..\n");
+     ifs.close();
+     return CURLE_READ_ERROR;
+  }
+  ifs.close();
+  return CURLE_OK;
+}
+
 size_t writer( char *ptr, size_t size, size_t nmemb, void *userdata) {
    size_t realsize=size*nmemb;
    std::string *out = reinterpret_cast<std::string *>(userdata);
@@ -12,12 +77,12 @@ size_t writer( char *ptr, size_t size, size_t nmemb, void *userdata) {
    return realsize;
 }
 
-CURLcode connectImap(std::string *buffer, std::string *Bearer) {
+CURLcode connectImap(std::string *buffer, std::string *Email, std::string *Bearer) {
   CURL *curl;
   CURLcode res = CURLE_OK;
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_USERNAME,"myemail@gmail.com");
+    curl_easy_setopt(curl, CURLOPT_USERNAME, Email->c_str());
     curl_easy_setopt(curl, CURLOPT_XOAUTH2_BEARER, Bearer->c_str());
     curl_easy_setopt(curl, CURLOPT_URL, "imaps://imap.gmail.com:993/INBOX");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);
@@ -29,14 +94,18 @@ CURLcode connectImap(std::string *buffer, std::string *Bearer) {
   return res;
 }
 
-CURLcode refreshToken(std::string * Bearer) {
+CURLcode refreshToken(std::string *ClientId, std::string *ClientSecret, std::string *RefreshToken, std::string *Bearer) {
   CURL *curl;
   CURLcode res = CURLE_OK;
   curl = curl_easy_init();
   if(curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "https://accounts.google.com/o/oauth2/token");
     curl_easy_setopt(curl, CURLOPT_POST, 1);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "client_id=myclientid&client_secret=myclientsecret&refresh_token=myrefreshtoken&grant_type=refresh_token");
+    std::stringstream cmd;
+    cmd<<"client_id="<<*ClientId<<"&client_secret="<<*ClientSecret;
+    cmd<<"&refresh_token="<<*RefreshToken<<"&grant_type=refresh_token";
+    std::string postcmd=cmd.str();
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postcmd.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);
     std::string buffer;
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
@@ -50,7 +119,6 @@ CURLcode refreshToken(std::string * Bearer) {
       }
       b>>tmp;
       *Bearer=tmp.substr(1,tmp.size()-3);
-      std::cout<<"New Bearer is: "<<*Bearer<<std::endl;
     }
   }
   curl_easy_cleanup(curl);
@@ -59,20 +127,27 @@ CURLcode refreshToken(std::string * Bearer) {
 
 void notifyByVoice(int ctr) {
   std::stringstream cmd;
-  cmd<<"say \"You have "<<ctr<<" new message"<<(ctr>1?"s":"")<<"!\"";
+  if(ctr>0) {
+    cmd<<"say \"You have "<<ctr<<" new message"<<(ctr>1?"s":"")<<"!\"";
+  }
   system(cmd.str().c_str());
 }
  
 int main(void)
 {
-  std::string str;
+  std::string str, Email, ClientId, ClientSecret, RefreshToken, Bearer;
   CURLcode res;
-  std::string bearer;
-  res = refreshToken(&bearer);
-  if(!res) {
-    std::cout<<"Refreshed the token successfully!"<<std::endl;
+  res = parseConfig(&Email, &ClientId, &ClientSecret, &RefreshToken);
+  if(res) {
+    printf("Could not parse the ginboxchecker.conf file. Exiting..\n");
+    return (int)res;
   }
-  res = connectImap(&str,&bearer);
+  res = refreshToken(&ClientId, &ClientSecret, &RefreshToken, &Bearer);
+  if(res) {
+    printf("Could not refresh the token. Exiting..\n");
+    return (int)res;
+  }
+  res = connectImap(&str, &Email, &Bearer);
   while(!res) {
     int tmp;
     int ctr=0;
@@ -84,10 +159,10 @@ int main(void)
     }
     notifyByVoice(ctr);
     sleep(300);
-    res = connectImap(&str,&bearer);
+    res = connectImap(&str, &Email, &Bearer);
   }
   if(res) {
-    printf("Something went wrong.\n");
+    printf("Could not connect to IMAP.\n");
   }
   return (int)res;
 }
